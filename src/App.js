a9b2713520d5449ba443a057463afb34a302d4de
Lewis Odero <lewisodero27@gmail.com>
2024-08-02 18:16:01 +0300

    style: add styles to the main page of the app

--- src/App.js
+++ src/App.js
@@ -1,51 +1,34 @@
-// import './App.css';
 import { useState } from 'react';
-import AddTodoForm from './AddTodoForm';
+import AddTodoForm from './components/AddTodoForm';
 import StateDemo from './StateDemo';
-import TodoList from './TodoList';
+import TodoList from './components/TodoList';
 
 const myTodos = [
-  {
-    id: 1,
-    title: 'todo one',
-    description: 'lorem ipsum lorem',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'todo two',
-    description: 'lorem ipsum lorem',
-    isComplete: true,
-  },
-  {
-    id: 3,
-    title: 'todo three',
-    description: 'lorem ipsum lorem',
-    isComplete: false,
-  },
-  {
-    id: 4,
-    title: 'todo four',
-    description: 'lorem ipsum lorem',
-    isComplete: true,
-  },
+  { id: 1, title: 'todo one', description: 'lorem ipsum lorem', isComplete: false },
+  { id: 2, title: 'todo two', description: 'lorem ipsum lorem', isComplete: true },
+  { id: 3, title: 'todo three', description: 'lorem ipsum lorem', isComplete: false },
+  { id: 4, title: 'todo four', description: 'lorem ipsum lorem', isComplete: true },
 ];
 
-// parent component of <TodoList />
 function App() {
   const [todos, setTodos] = useState(myTodos);
 
-  // func that handles adding a newTodo:{} and pass it as a prop to the AddTodoForm
   const addTodo = (newTodo) => {
     setTodos([...todos, newTodo]);
   };
 
   return (
-    <div className='App'>
-      <StateDemo />
-      <h1>todo tutorial</h1>
-      <AddTodoForm addTodo={addTodo} />
-      <div id='todolist'>
+    <div className='App container mx-auto p-4'>
+      <h1 className='text-3xl font-bold text-center mb-4'>Todo Tutorial</h1>
+      <div className='flex flex-col lg:flex-row lg:space-x-8'>
+        <div className='flex-1 flex items-stretch'>
+          <AddTodoForm addTodo={addTodo} className='flex-1' />
+        </div>
+        <div className='flex-1 flex items-stretch'>
+          <StateDemo className='flex-1' />
+        </div>
+      </div>
+      <div id='todolist' className='mt-8'>
         <TodoList todos={todos} />
       </div>
     </div>
